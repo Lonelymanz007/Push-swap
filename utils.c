cc39f142a24a2f36f21468f263ea1bdc564bfbe9
Teeraphat Phuwiang <bookmacbook@Book-MacBook-Air.local>
2026-07-13 20:40:37 +0700

    2 upload

--- utils.c
+++ utils.c
@@ -94,10 +94,10 @@ int	is_valid_number(char *str)
 	int	i;
 
 	i = 0;
-	if (str[i] == '\0')
-		return (0);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
+	if (str[i] == '\0')
+		return (0);
 	while (str[i])
 	{
 		if (str[i] < '0' || str[i] > '9')
